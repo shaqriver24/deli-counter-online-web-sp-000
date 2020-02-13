51da833459aec548fc3c6f4e21c421dc7047fbed
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-13 09:10:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 # Write your code here.
 def line(arr)
-  puts "The line is currently empty." if arr.length == 0
+  if arr.length == 0
+    puts "The line is currently empty."
+    return
+  end
   current_line = arr.each_with_index.map { |name, idx| "#{idx+1}. #{name}"}.join(" ")
   puts "The line is currently: #{current_line}"
 end
